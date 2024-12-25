d23d456d5d6f92cb451955fd07d30f61bcc1f4cf
neshanth <59483685+neshanth@users.noreply.github.com>
2024-12-25 07:49:25 +0530

    added methods to remove first and last node

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -46,6 +46,35 @@ class LinkedList {
         newNode.next = currentHeadNode
         this.head = newNode
     }
+
+    removeFirstNode() {
+        if (!this.head) {
+            return
+        }
+
+        let currentHeadNode = this.head
+        this.head = currentHeadNode.next
+        currentHeadNode = null
+    }
+
+    removeLastNode() {
+        if (!this.head) {
+            return
+        }
+        let node = this.getPreviousNode(this.tail)
+        node.next = null
+        this.tail = node
+    }
+
+    getPreviousNode(node) {
+        let current = this.head
+        let prev = null
+        while (current !== node) {
+            prev = current
+            current = current.next
+        }
+        return prev
+    }
 }
 
 const l1 = new LinkedList()
@@ -53,5 +82,7 @@ l1.append(10)
 l1.append(20)
 l1.append(30)
 l1.prepend(5)
+l1.removeFirstNode()
+l1.removeLastNode()
 l1.print()
 console.log(l1);
